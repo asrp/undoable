610c35b17bdf7485f28672bacf55f65296d22987
asrp <asrp@email.com>
2017-01-28 20:45:26 +0100

    Add barebones setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='undoable',
+      version='0.3',
+      description='Simple Python list and dict with undo/redo',
+      long_description='A simple Python list and dict with callbacks when they are changed and undo/redo using these callbacks',
+      url='https://github.com/asrp/undoable',
+      author='asrp',
+      author_email='asrp@email.com',
+      py_modules=['undoable'],
+      keywords='callbacks undo observer')
